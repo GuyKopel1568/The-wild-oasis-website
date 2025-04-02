122f4feec52ac3a5ccf0dd03bff0802cecc0cd5b
GuyKopel1568 <guykopel1568@gmail.com>
2025-04-02 19:49:13 +0300

    Interactivity with client components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,10 @@
+import Counter from '../components/Counter';
+
 export default async function Page() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
   console.log(data);
+
   return (
     <div>
       <h1>Cabins Page</h1>;
@@ -10,6 +13,7 @@ export default async function Page() {
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
+      <Counter users={data} />
     </div>
   );
 }
