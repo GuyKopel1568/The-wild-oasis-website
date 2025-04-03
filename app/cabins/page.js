89a9476b386faedf50af238128d061ff3c2dce50
GuyKopel1568 <guykopel1568@gmail.com>
2025-04-03 10:01:31 +0300

    Adding page metadata and favicon

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,11 @@
-import Counter from '@/app/_components/Counter';
+export const metadata = {
+  title: 'Cabins',
+};
 
 export default async function Page() {
   return (
     <div>
       <h1>Cabins Page</h1>
-      <ul></ul>
-      <Counter users={data} />
     </div>
   );
 }
