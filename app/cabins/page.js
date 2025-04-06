5f55cd05e1efef03185ec69bd7619618a8a03695
GuyKopel1568 <guykopel1568@gmail.com>
2025-04-06 16:42:34 +0300

    Dynamic routes and building the cabin page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,7 +2,7 @@ import CabinList from '@/app/_components/CabinList';
 import { Suspense } from 'react';
 import Spinner from '@/app/_components/Spinner';
 
-export default async function Page() {
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
