a43286b3acefbc9bd0a76cbcc5b65f1627cb9777
GuyKopel1568 <guykopel1568@gmail.com>
2025-04-06 16:54:22 +0300

    Generating dynamic metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,15 @@ import { getCabin } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
+export async function generateMetadata({ params }) {
+  const cabin = await getCabin(params.cabinId);
+  const { name, description } = cabin;
+  return {
+    title: `Cabin ${name} `,
+    description: description,
+  };
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
