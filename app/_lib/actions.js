36c2a08836432ad574eb804c150eed717d682f70
GuyKopel1568 <guykopel1568@gmail.com>
2025-04-08 13:47:09 +0300

    Add sign out

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,4 +1,10 @@
+import { redirect } from 'next/navigation';
+
 export async function signInAction() {
   // this won't work for popup
   redirect('/api/auth/signin/google?callbackUrl=/account');
+}
+
+export async function signOutAction() {
+  redirect('/api/auth/signout?callbackUrl=/');
 }
